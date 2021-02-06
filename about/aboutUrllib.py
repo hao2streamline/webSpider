3f24fbf2d2c68428ae09266b70c648a8f076fd56
Hao2steamline <hao2streamline@users.noreply.github.com>
2021-02-07 01:38:48 +0800

    Initial commit

--- about/aboutUrllib.py
+++ about/aboutUrllib.py
@@ -43,10 +43,10 @@ import urllib.request
 # print(response.read().decode("utf-8"))
 
 
-# url = "http://douban.com"
-# heders = {
-#     "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
-# }
-# req = urllib.request.Request(url=url,headers=heders,method="GET")
-# response = urllib.request.urlopen(req)
-# print(response.read().decode("utf-8"))
+url = "http://douban.com"
+heders = {
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
+}
+req = urllib.request.Request(url=url,headers=heders,method="GET")
+response = urllib.request.urlopen(req)
+print(response.read().decode("utf-8"))
